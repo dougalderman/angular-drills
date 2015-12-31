1380b086c04289b70517ecf495a61538e5a6e788
Doug Alderman <agnadcp-d1@yahoo.com>
2015-12-31 09:29:37 -0700

    app4 complete

--- app4 - api/controller.js
+++ app4 - api/controller.js
@@ -1,14 +1,38 @@
 angular.module('apiApp').
-controller('apiController', function ($scope, apiService) {
+controller('apiController', function ($scope, $sce, apiService) {
 
   $scope.getTheBlog = function(url) {
+        $scope.blog = {};
+        $scope.year = '';
+        $scope.posts = [];
+        if (url.slice(0, 7) === 'http://')
+          url = url.slice(7);
         apiService.getBlog(url)
         .then(function(data) {
           if (data)  {
             $scope.blog = data;
+            $scope.getYearPublished();
             console.log(data);
           }
       })
+  };
+
+  $scope.getYearPublished = function() {
+    $scope.year = $scope.blog.published.slice(0, 4);
+  };
+
+  $scope.getPosts = function() {
+    apiService.getPosts($scope.blog.id)
+    .then(function(data) {
+      if (data)  {
+        $scope.posts = data.items;
+        console.log(data.items);
+      }
+    })
+  };
+
+  $scope.renderHtml = function(html_code) {
+    return $sce.trustAsHtml(html_code);
   };
 
   })
--- app4 - api/service.js
+++ app4 - api/service.js
@@ -5,7 +5,29 @@ service('apiService', function ($http) {
     return $http({
       method: 'GET',
       url: 'https://www.googleapis.com/blogger/v3/blogs/byurl?url=http%3A%2F%2F'
-       + url + '&key=AIzaSyA76zA2_gHDMWO5ovldnRBm2wPXSfowM0M'
+       + url + '&key=AIzaSyAVFH5xx0vrXvH3joiITI_e87HRTTGpNtc'
+    })
+    .then(function(response) {
+      if (response.status === 200) {
+        return response.data;
+      }
+      else {
+        console.log('status code is ' + response.status)
+        return response.status;
+      }
+    })
+    .catch(function(err) {
+      console.log('Error code: ' + err.status);
+      console.log(err);
+    });
+
+  };
+
+  this.getPosts = function(blogId) {
+    return $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/blogger/v3/blogs/' +
+      blogId + '/posts?key=AIzaSyAVFH5xx0vrXvH3joiITI_e87HRTTGpNtc'
     })
     .then(function(response) {
       if (response.status === 200) {
